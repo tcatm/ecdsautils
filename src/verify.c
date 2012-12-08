e4c8517e1187e26d28f4e088fa0cd10cee216d15
Nils Schneider <nils.schneider@gmail.com>
2012-12-08 04:16:57 +0100

    move pubkey verification to is_valid_pubkey()

--- src/verify.c
+++ src/verify.c
@@ -32,12 +32,22 @@
 
 #include "hexutil.h"
 
-static const unsigned char q[32] = {
+const unsigned char q[32] = {
   0xed, 0xd3, 0xf5, 0x5c, 0x1a, 0x63, 0x12, 0x58,
   0xd6, 0x9c, 0xf7, 0xa2, 0xde, 0xf9, 0xde, 0x14,
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10
 };
+
+int is_valid_pubkey(ecc_25519_work *pubkey) {
+  ecc_25519_work work;
+
+  // q * pubkey should be identity element
+  ecc_25519_scalarmult(&work, *(const void*[]){q}, pubkey);
+
+  // FIXME: Check whether pubkey lies on curve?
+  return ecc_25519_is_identity(&work) && !ecc_25519_is_identity(pubkey);
+}
 
 void main(int argc, char *argv[]) {
   unsigned char signature[64];
@@ -64,16 +74,8 @@ void main(int argc, char *argv[]) {
 
   ecc_25519_load_packed(&pubkey, &pubkey_packed);
 
-  if (ecc_25519_is_identity(&pubkey))
-    error(1, 0, "Pubkey must not be identity element!");
-
-  // FIXME: Check whether pubkey lies on curve?
-
-  // Check whether q * pubkey = identity element
-  memcpy(tmp.p, q, 32);
-  ecc_25519_scalarmult(&work, &tmp, &pubkey);
-  if (!ecc_25519_is_identity(&work))
-    error(1, 0, "Invalid public key");
+  if (!is_valid_pubkey(&pubkey))
+    error(1, 0, "Invalid pubkey");
 
   ecc_25519_gf_recip(&tmp, &s);
   ecc_25519_gf_reduce(&w, &tmp);
